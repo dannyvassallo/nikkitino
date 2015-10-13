293e03333e62d30546c9a88e935e6013daea089b
dannyvassallo <danielvassallo87@gmail.com>
2015-10-13 16:51:52 -0400

    added gate and scrolltotop on load

---
+++ source/javascripts/_age-gate.js
@@ -0,0 +1,18 @@
+function validateAge() {
+    var Bdate = document.forms["age-gate"]["age"].value;
+    var Bday = +new Date(Bdate);
+
+    var age = (Date.now() - Bday) / (31557600000);
+    if ( age < 13 ) {
+        alertify.error('Sorry, but you are not eligible for this sweepstakes.');
+        return false
+    }
+    else if ( $.isNumeric(age) == false ){
+        alertify.error('Sorry, your age must be equal to a number.');
+        return false
+    }
+    else{
+        $('#age-gate').fadeOut();        
+        return false
+    }
+}
--- source/javascripts/all.js
+++ source/javascripts/all.js
@@ -2,5 +2,7 @@
 //= require bootstrap-sprockets
 //= require bootstrap
 //= require vendor/_alertify
+//= require_age-gate
 //= require_validate
+//= require_scrolltop
 //= require_tree .
